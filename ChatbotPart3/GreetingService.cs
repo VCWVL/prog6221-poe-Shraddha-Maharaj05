f05ecc7c70e66a196679212ac3e71dfd2a1d8f98
Shraddha-Maharaj05 <ST10450514@vcconnect.edu.za>
2025-06-26 19:04:31 +0200

    initial commit
    Prog Part 3 POE

---
+++ ChatbotPart3/GreetingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Media;
+
+namespace ChatbotPart3
+{
+    public class GreetingService
+    {
+        public string GetWelcomeMessage()
+        {
+            return "Welcome to CyberBot!";
+        }
+
+        public void PlayWelcomeSound()
+        {
+            try
+            {
+                // Adjust path as needed or embed resource
+                var player = new SoundPlayer("welcome.wav");
+                player.Play();
+            }
+            catch
+            {
+                // Handle or ignore missing sound file gracefully
+            }
+        }
+    }
+}
